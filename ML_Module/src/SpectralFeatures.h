7f71f17cf7795945280a1a63750792530a1e82ed
C. Latina <chrisrlatina@gmail.com>
2017-04-20 13:40:41 -0700

    Update SpectralFeatures.h

--- ML_Module/src/SpectralFeatures.h
+++ ML_Module/src/SpectralFeatures.h
@@ -2,9 +2,13 @@
 //  SpectralFeatures.h
 //  module
 //
-//  Created by Christopher Latina on 11/24/15.
-//  Copyright © 2016 Christopher Latina. All rights reserved.
-//
+//Copyright (c) 2015-2017 Christopher Latina
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, and / or merge, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// This software is protected under Creative Commons Attribution-NonCommercial-ShareAlike. Please respect this commitment
+// https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 
 #ifndef SpectralFeatures_hpp
 #define SpectralFeatures_hpp
